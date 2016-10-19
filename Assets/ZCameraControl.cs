96cf0f5a274e05a0262962e0abc2362586c95988
Stephen Spiteri <strospiteri@gmail.com>
2016-10-19 17:54:04 +0100

    Update

---
+++ Assets/ZCameraControl.cs
@@ -0,0 +1,99 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ZCameraControl : MonoBehaviour
+{
+    /*
+     *      The Z Camera is a camera system which follows the player.
+     *      
+     *      The camera is located behind the player with an elevation above the floor, 
+     *      In addition the camera will always looks at the player. 
+     *      
+     *      The camera's position is updated over time, so it has to catch up to the player.
+     */
+
+    private float zCameraDistance   = 5f;
+    private float zCameraDistanceUp = 5f;    
+    private float zCameraSmoothness = 0.1f;   //The smoothness of the camera's transitions
+
+    private Transform   zCameraFollow;        //The object the camera is following
+    private Vector3     zCameraTargetPosition;//The desired position of the camera;
+
+    private Vector3 zCameraLookDirection;
+
+    private Vector3 velocity = Vector3.zero;
+
+    private Vector3 zCameraOffset = new Vector3(0f, 1.5f, 0f);
+
+    #region Methods
+
+    //Finds the player character within the current scene
+    private void zCameraFindPlayer() 
+    {
+        zCameraFollow = GameObject.FindWithTag("Player").transform;
+    }
+
+    //Updates the position of the camera relative to the player
+    private void zCameraUpdate()
+    {
+        if (zCameraFollow)
+        {
+            //The camera's position near the  character is offset
+            Vector3 characterOffset = zCameraFollow.position + zCameraOffset;
+            zCameraLookDirection = characterOffset - this.transform.position;
+
+            //Get the camera's direction
+            zCameraLookDirection.y = 0;
+            zCameraLookDirection.Normalize();
+
+            /*
+             * The desired position of the camera is calculated
+             * This uses the position of the player, 
+             * The camera is also elevated off the floor by an offset using 'zCameraDistanceUp'
+             * The camera is placed [???] with a distance offset using 'zCameraDistance'
+             */
+            zCameraTargetPosition = (characterOffset + zCameraFollow.up * zCameraDistanceUp ) - (zCameraLookDirection * zCameraDistance);
+
+
+            /*
+             * The camera's position is updated gradually to the intended position in space
+             */
+            zCameraSmoothPosition(this.transform.position, zCameraTargetPosition);
+
+            transform.LookAt(zCameraFollow);
+        }
+    }
+
+    void zCameraSmoothPosition(Vector3 fromPos, Vector3 toPos)
+    {
+        this.transform.position = Vector3.SmoothDamp(fromPos, toPos, ref velocity, zCameraSmoothness);
+    }
+
+    //Draw/Print Debug Information related to the camera system
+    private void zCameraDebug()
+    {
+        //Height of camera away from floor
+        Debug.DrawRay(zCameraFollow.position, Vector3.up * zCameraDistanceUp, Color.red);
+
+        //Distance of camera behind player
+        Debug.DrawRay(zCameraFollow.position, -1f * zCameraFollow.forward * zCameraDistance, Color.blue);
+
+        //The bee-line which the camera will have to travel to between it's old and new position
+        Debug.DrawLine(zCameraFollow.position, zCameraTargetPosition, Color.magenta);
+    }
+
+    
+    void Start()
+    {
+        zCameraFindPlayer();
+    }
+
+    void LateUpdate()
+    {
+        zCameraUpdate();
+
+        //zCameraDebug();
+    }
+
+    #endregion
+}
---
+++ Assets/ZCharacterControl.cs
@@ -0,0 +1,127 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ZCharacterControl : MonoBehaviour
+{
+    #region Variables
+    [SerializeField]
+    private Animator zCharAnimator;
+    [SerializeField]
+    private float directionDampTime = .25f;
+
+    private float zCharSpeed = 0.0f;
+    private float zCharDirection = 0f;
+    private float zCharH = 0.0f;
+    private float zCharV = 0.0f;
+    private float zCharTurnSpeed = 5f;
+
+    [SerializeField]
+    private ZCameraControl zCamera;
+
+
+    #endregion
+
+    //Method used for updating the state of the animator
+
+    private void zCharAnimation()
+    {
+        //Only works if animator exists
+        if (zCharAnimator)
+        {
+            //Get Axis Inputs
+            zCharH = Input.GetAxis("Horizontal");
+            zCharV = Input.GetAxis("Vertical");
+
+            /*
+             * This method calculates both the characters movement direction and speed
+             * Using references, it makes it possible to calculate to values at once instead of returning a single value             * 
+             */
+            zCharInputToWorldSpace(this.transform, zCamera.transform, ref zCharDirection, ref zCharSpeed);
+
+            //Set the animator values based on this new information
+            zCharAnimator.SetFloat("Speed", zCharSpeed);
+            zCharAnimator.SetFloat("Direction", zCharDirection, directionDampTime, Time.deltaTime);
+
+
+        }
+    }
+
+    void zCharInputToWorldSpace(Transform root, Transform camera, ref float directionOut, ref float speedOut)
+    {
+        //Get the character's forward facing direction
+        Vector3 rootDirection = root.forward;
+
+        //Get the input direction and magnitude
+        Vector3 stickDirection = new Vector3(zCharH, 0, zCharV);
+        speedOut = stickDirection.sqrMagnitude;
+
+        //Get the forward facing direction of the camera
+        Vector3 cameraDirection = camera.forward;
+
+        //Kill the Y Axis of the camera's direction since the stick direction doesnt have a y rotation
+        cameraDirection.y = 0.0f;
+
+        /*
+         * The rotation difference is calculated between the camera direction and the forward facing direction
+         * This is done to get a rotation 'shift' to be applied to the input direction
+         */
+        Quaternion shift = Quaternion.FromToRotation(Vector3.forward, cameraDirection);
+
+        /*
+         * The input direction is offset by this shift rotation using quaternion multiplication, effectively 'adding' the angles together
+         * This results in an inpt direction relative to the camera for camera relative movement
+         * 
+         * When the stick is pushed forwards, the direction is away from the camera, 
+         * where as the stick pushed backwards is towards it.
+         */
+        Vector3 moveDirection = shift * stickDirection;
+
+        /*
+         * Find the direction in between the two directions 
+         * (i.e a vector perpendicular to the other two using left hand rule 
+         * 
+         * Direction A = thumb, 
+         * Direction B = Index Finger
+         * 
+         * This is used to heelp determine the 'direction of rotation' whilst changing direction of movement
+         */
+        Vector3 axisSign = Vector3.Cross(moveDirection, rootDirection);
+
+        /*
+         * Get the angle between the characters facing direction and desired movement direction
+         * Using the axis sign, determine the direction of rotation
+         * Multiply this result (-1 or 1) by the angle
+         * 
+         * This results in the size of the rotation the character will turn and in which direction
+         * 
+         * Dividing this value by 180 (i.e a hemisphere), the result is a value between -1 and 1!
+         */
+        float angleRootToMove = Vector3.Angle(rootDirection, moveDirection) * (axisSign.y >= 0 ? -1f : 1f);
+        angleRootToMove /= 180f;
+
+        //Finally multiply the final direction of rotation (between -1 and 1) with the characters turn speed.
+        directionOut = angleRootToMove * zCharTurnSpeed;
+
+    }
+
+
+    // Use this for initialization
+    void Start()
+    {
+        //Setup Animator
+        zCharAnimator = GetComponent<Animator>();
+        if (zCharAnimator.layerCount >= 2)
+            zCharAnimator.SetLayerWeight(1, 1);
+    }
+
+    void FixedUpdate()
+    {
+        //DO STUFF HERE NEXT
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        zCharAnimation();
+	}
+}
